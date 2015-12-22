a4c63427361d9537a6c29aeea98bf5865d11280e
S. Gabbert <sebastian.gabbert@web.de>
2015-12-22 16:55:59 +0100

    removed unnessecary folders

--- JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
+++
@@ -1,4 +0,0 @@
-// This is an auto-generated file to redirect any included
-// module headers to the correct external folder.
-
-#include "C:\juce-grapefruit-windows\modules\juce_audio_plugin_client\juce_audio_plugin_client.h"
